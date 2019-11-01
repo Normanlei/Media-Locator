6c566c7366a54176a588ee5348cb397229c05258
Kami <51175640+hfurusho@users.noreply.github.com>
2019-10-31 19:42:03 -0700

    Merged features to load events after search.

--- Final_version/js/harrys-script.js
+++ Final_version/js/harrys-script.js
@@ -1,52 +1,66 @@
 const TDkey = "348716-WhatsAro-8WWCXOC7";
+let recommends;
 
 $("#search_open,#search_close").on("click", function(event) {
   event.preventDefault();
   // console.log($("#query_open").val);
   // console.log($("#location_open").val);
-  if ($("#query_open").val().length>0 && $("#location_open").val().length>0){
-  $("#index-banner").css('display','none');
-  $("#function-section").css('display','block');
-  $("#foot-setion").css('display','none');
-  getPlaceDetailViaAddress($("#location_open").val().trim());
-  //getResults();
-  }else {
+  if (
+    $("#query_open").val().length > 0 &&
+    $("#location_open").val().length > 0
+  ) {
+    $("#index-banner").css("display", "none");
+    $("#function-section").css("display", "block");
+    $("#foot-setion").css("display", "none");
+    getPlaceDetailViaAddress(
+      $("#location_open")
+        .val()
+        .trim()
+    );
+    getResults();
+  } else {
     alert("need more parameter"); //need more works
   }
 });
 
 function getResults() {
   let search = $("#query_open").val();
-  let type = "music"; //change
   let moreInfo = 1;
+  recommends = [];
   console.log(search, type, moreInfo);
-  let tasteDiveURL = `http://tastedive.com/api/similar?q=${search}&type=${type}&info=${moreInfo}&limit=10&k=${TDkey}`;
+  let tasteDiveURL = `http://tastedive.com/api/similar?q=${search}&info=${moreInfo}&limit=20&k=${TDkey}`;
   $.ajax({
     url: tasteDiveURL,
-    type: "GET", 
+    type: "GET",
     dataType: "jsonp"
   }).then(function(response) {
-    console.log(response);
     populateResults(response);
   });
 }
 
 function populateResults(response) {
   $("#results").empty();
-  let info = response.Similar.Info[0];
-  let results = response.Similar.Results;
-  $("#search-span").text('Search: '+info.Name+"    ");
-  $("#type-span").text('Type: ' +info.Type);
+  if (response.Similar.Results.length > 0) {
+    let info = response.Similar.Info[0];
+    let results = response.Similar.Results;
+    let searchedItem = $("<li class='collection-item'>");
+    searchedItem.text(`Name: ${info.Name}, Type: ${info.Type}`);
+    searchedItem.attr("data-name", info.Name);
+    recommends.push(info.Name);
+    $("#results").append(searchedItem);
 
-  for (let i = 0; i < results.length; i++) {
-    let li = $("<li class='collection-item'>");
-    li.text(`Name: ${results[i].Name}, Type: ${results[i].Type}`);
-    li.attr("data-name", results[i].Name);
+    for (let i = 0; i < results.length; i++) {
+      let li = $("<li class='collection-item'>");
+      li.text(`Name: ${results[i].Name}, Type: ${results[i].Type}`);
+      li.attr("data-name", results[i].Name);
+      $("#results").append(li);
+      recommends.push(results[i].Name);
+    }
+  } else {
+    let li = $("<li class='list-group-item td-item'>").text("No Results");
     $("#results").append(li);
   }
-  $(".collection-item").on("click", function() {
-    getEvents($(this).attr("data-name"), $(this));
-  });
+  initGetEvents();
 }
 
 /*************************
@@ -57,14 +71,30 @@ TICKETMASTER API SECTION
 
 const TMKey = "cNBbi9N6U5RzvrGZpA7IcJX4CIKWyN6G";
 
+// Initializes the ticketmaster event finder and limits the API calls.
+function initGetEvents() {
+  function delay(interval) {
+    return new Promise(resolve => setTimeout(resolve, interval));
+  }
+  async function limitApiCalls() {
+    for (let i = 0; i < recommends.length; i++) {
+      await delay(225);
+      getEvents(recommends[i], $('li[data-name="' + recommends[i] + '"]'));
+    }
+  }
+  limitApiCalls();
+}
+
 function getEvents(name, liEle) {
   let ticketMasterURL = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${TMKey}&keyword=${name}&city=seattle`;
-  console.log(name, ticketMasterURL);
+  // &radius=50&unit=miles&latlong={YOURCOORDS}&size=40&classificationName=[Music, Arts & Theatre]
+  // {YOURCOORDS} is latitude,longitude (ex. -132.01,23.22) NO BRACKETS
+  // REMOVE &city=seattle if using latlong
   $.ajax({
     type: "GET",
-    url: ticketMasterURL
+    url: ticketMasterURL,
+    dataType: "json"
   }).then(function(response) {
-    console.log("tmResponse", response);
     if (response.page.totalElements != 0) {
       populateNearbyEvents(response, name, liEle);
     } else {
@@ -74,13 +104,17 @@ function getEvents(name, liEle) {
   });
 }
 
-function populateNearbyEvents(response, name, liEle) {
+function populateNearbyEvents(response, name) {
   let event = response._embedded.events[0];
   console.log(name);
   console.log(event);
   console.log(response);
   let coords = event._embedded.venues[0].location;
-  getFinalPlaceDetail(parseFloat(coords.latitude),parseFloat(coords.longitude),event._embedded.venues[0].name);
+  getFinalPlaceDetail(
+    parseFloat(coords.latitude),
+    parseFloat(coords.longitude),
+    event._embedded.venues[0].name
+  );
   //createMarker(mycurrLatLng);
   // let spanCoords = $("<span>");
   // spanCoords.text(" " + coords.latitude + " " + coords.longitude);
