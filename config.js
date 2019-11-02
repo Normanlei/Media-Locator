2420689010b553d0faa36a243013f030e29e0411
Norman Lei <normanlei0901@gmail.com>
2019-11-02 10:20:17 -0700

    config

---
+++ config.js
@@ -0,0 +1,4 @@
+var config = {
+  yelp_api_key: "tr-rutoaUmQmJY4O9eaGZEayHcG5z8G9He0VyMHhh8TahC-oimO5EuPnaSrEAFo4UqZVlLpwRYA09S29Y7ba0Ome1uPgQ2HXsATuHV5dj2kxggxHlMmgzzg1a-S3XXYx", 
+  google_api_key: 'AIzaSyCy_fsa23_HL03eeIZhtAcxDd8RCmVnogo'
+  }
